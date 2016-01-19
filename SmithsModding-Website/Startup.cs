043247908bb3e340511dcb2990f2ea7f3fa4957a
OrionDevelopment <OrionDevelopment@outlook.com>
2016-01-19 16:17:10 +0100

    Initial Commit

---
+++ SmithsModding-Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmithsModding_Website.Startup))]
+namespace SmithsModding_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
