5cdffb625321855b295cf5f1de5345876a9e6890
OrionDevelopment <OrionDevelopment@outlook.com>
2016-01-19 20:03:54 +0100

    Started working.

--- SmithsModding-Website/Models/AccountViewModels.cs
+++ SmithsModding-Website/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace SmithsModding_Website.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username{ get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -68,6 +67,11 @@ namespace SmithsModding_Website.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
