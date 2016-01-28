df5005bc831b12c54ab273f2c145d48edc8c5cc5
OrionDevelopment <OrionDevelopment@outlook.om>
2016-01-28 18:24:40 +0100

    First working version of the Edit. Still some work todo, but you can edit News Posts now.

--- SmithsModding-Website/Controllers/HomeController.cs
+++ SmithsModding-Website/Controllers/HomeController.cs
@@ -26,27 +26,7 @@ namespace SmithsModding_Website.Controllers
             ViewBag.Logo = "~/Content/res/Logo.jpg";
             ViewBag.Header = true;
 
-            var nim = new NewsViewModel();
-
-            using (ApplicationDbContext db = new ApplicationDbContext())
-            {
-                //Grab only the first 20 post orso.
-                int lastPostIndex = 19;
-
-                //If we donnot have 20 posts yet, grab as many as we have.
-                if ((await db.NewsItems.CountAsync()) < 20)
-                {
-                    lastPostIndex = (await db.NewsItems.CountAsync());
-                }
-
-                //Grab the determined amount of posts (use include to grab a relation ship, cause LazyLoading is disabled)
-                nim.Items = (await db.NewsItems.Include(u => u.Author).ToListAsync()).GetRange(0, lastPostIndex);
-
-                //Sort them based on the release date: mulitply with -1 inverts the sort order
-                //That makes it so that the newest post land ontop and the oldest on the bottom.
-                nim.Items.Sort((x, y) => x.PublishDate.CompareTo(y.PublishDate) * -1);
-            }
-
+            var nim = await getStandardNewsDisplayModel();
             //Return the view.
             return View(nim);
         }
@@ -79,6 +59,23 @@ namespace SmithsModding_Website.Controllers
             return RedirectToAction("News", "Home");
         }
 
+        [HttpPost]
+        [Authorize(Roles = "Administrators")]
+        [ValidateAntiForgeryToken]
+        [ActionName("EditNews")]
+        public async System.Threading.Tasks.Task<ActionResult> EditNews(NewsViewModel model)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                model.editNewsItem.Author = await new UserManager<ApplicationUser, string>(new UserStore<ApplicationUser>(db)).FindByIdAsync(User.Identity.GetUserId());
+                
+                db.Entry(model.editNewsItem).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+            }
+
+            return RedirectToAction("News", "Home");
+        }
+
         [Authorize(Roles = "Administrators")]
         [ActionName("DeleteNewsItem")]
         public async System.Threading.Tasks.Task<ActionResult> DeleteNewsItem(string id)
@@ -108,17 +105,47 @@ namespace SmithsModding_Website.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
+            var nim = await getStandardNewsDisplayModel();
+
             using (ApplicationDbContext db = new ApplicationDbContext())
             {
-                var nim = new NewsViewModel();
-                NewsItem news = await db.NewsItems.FindAsync(id);
-                if (news == null)
+                NewsItem editItem = await db.NewsItems.FindAsync(id);
+                if (editItem == null)
                 {
-                    return RedirectToAction("News", "Home");
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
                 }
-                nim.editNewsItem = news;
-                return PartialView("_EditNewsPartial", nim);
+
+                nim.editNewsItem = editItem;
             }
+
+            return View("News", nim);
+        }
+
+        private async System.Threading.Tasks.Task<NewsViewModel> getStandardNewsDisplayModel()
+        {
+            var nim = new NewsViewModel();
+
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                //Grab only the first 20 post orso.
+                int lastPostIndex = 19;
+
+                //If we donnot have 20 posts yet, grab as many as we have.
+                if ((await db.NewsItems.CountAsync()) < 20)
+                {
+                    lastPostIndex = (await db.NewsItems.CountAsync());
+                }
+
+                //Grab the determined amount of posts (use include to grab a relation ship, cause LazyLoading is disabled)
+                nim.Items = (await db.NewsItems.Include(u => u.Author).ToListAsync()).GetRange(0, lastPostIndex);
+
+                //Sort them based on the release date: mulitply with -1 inverts the sort order
+                //That makes it so that the newest post land ontop and the oldest on the bottom.
+                nim.Items.Sort((x, y) => x.PublishDate.CompareTo(y.PublishDate) * -1);
+            }
+
+            return nim;
         }
     }
 }
